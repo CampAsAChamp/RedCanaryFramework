24f796768548bdcc7fdcfa97e8eba0c6d45b7ece
Nick Schneider <nickschneider101@gmail.com>
2022-01-05 10:17:54 -0800

    Added server for testing connect and send

---
+++ server.py
@@ -0,0 +1,11 @@
+def receive():
+    pass
+
+# Run this script separately from the framework to be able to test the send() method
+def main():
+    print("Hello World")
+
+# Press the green button in the gutter to run the script.
+# Only runs if the file is being run as a script, not being imported as a module
+if __name__ == '__main__':
+    main()
